09d56d0863c8bb3a9831cf02ba1d1c92cb6546b7
Kevin Lyons <zinckev@hotmail.com>
2021-02-10 15:46:16 -0800

    fixing 0 data load error

--- public/service-worker.js
+++ public/service-worker.js
@@ -2,6 +2,7 @@ const CACHE_NAME = "static-cache-v2";
 const DATA_CACHE_NAME = "data-cache-v1";
 const staticFilesToPreCache = [
   "/",
+  "/index.html",
   "/db.js",
   "/index.js",
   "/styles.css",
